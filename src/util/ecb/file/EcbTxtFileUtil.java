7b1a47d27d66c302e6bf3f7f60849145e743af77
Asriel Canté <asriel.cante@gmail.com>
2017-10-20 16:50:48 -0500

    organiza imports

--- src/util/ecb/file/EcbTxtFileUtil.java
+++ src/util/ecb/file/EcbTxtFileUtil.java
@@ -6,7 +6,6 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;

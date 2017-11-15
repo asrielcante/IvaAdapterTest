ff34f967c5e637caff64e31cf3ef15dbefd98a8a
Asriel Canté <asriel.cante@gmail.com>
2017-11-15 18:13:16 -0500

    ajuste al shell de prurba

--- resources/ecbIvaTest.sh
+++ resources/ecbIvaTest.sh
@@ -21,7 +21,7 @@ echo $fecha
 timestamp=$(date +"%H%M%S");
 echo $timestamp
 
-#rutas
+#rutas formatea
 pathECBEntrada="/home/linuxlite/shell_scripts/ECBIVA/interfaces/";
 pathECBSalida="/home/linuxlite/shell_scripts/ECBIVA/CFDProcesados/";
 pathECBCatalogos="/home/linuxlite/shell_scripts/ECBIVA/interfaces/";
@@ -34,50 +34,56 @@ validaSalidas(){
 	nombreA="$1"
 	fechaA="$2"
 	timeStampA="$3"
-	
-	if [ "$nombreA" == "CFDPTCARTER" ] || [ "$nombreA" == "CFDPTSOFOMC" ]  ; then
+	returnVal=0
+	if [ "$nombreA" == "CFDPTCARTER" ] || [ "$nombreA" == "CFDPTSOFOMC" ] ; then
 		##valida salida carter
 		if [ ! -e "${pathECBSalida}${nombreA}${fechaA}ORIGINAL_CARTER_${timeStampA}.TXT" ] ; then
-			echo "Error:falta archivo de salida ORIGINAL_CARTER_ para ${nombreA}${fechaA}, se detiene el proceso";
-			exit -1
+			echo "Error:falta archivo de salida ORIGINAL_CARTER_ para ${nombreA}${fechaA}";
+			returnVal=1
 		fi
 	else
-		if [ "$nombreA" == "CFDLMPAMPAS" ] || [ "$nombreA" == "CFDLMPAMPAA" ]  ; then
+		if [ "$nombreA" == "CFDLMPAMPAS" ] || [ "$nombreA" == "CFDLMPAMPAA" ] ; then
 			#valida salida pampa
 			if [ ! -e "${pathECBSalida}${nombreA}${fechaA}ORIGINAL_PAMPA_${timeStampA}.TXT" ] ; then
-				echo "Error:falta archivo de salida ORIGINAL_PAMPA_ para ${nombreA}${fechaA}, se detiene el proceso";
-				exit -1
+				echo "Error:falta archivo de salida ORIGINAL_PAMPA_ para ${nombreA}${fechaA}";
+				returnVal=1
 			fi
 		fi
 		
 		#valida salida control
 		if [ ! -e "${pathECBSalida}${nombreA}${fechaA}_CONTROL_${timeStampA}.TXT" ] ; then
-			echo "Error:falta archivo de salida _CONTROL_ para ${nombreA}${fechaA}, se detiene el proceso";
-			exit -1
+			echo "Error:falta archivo de salida _CONTROL_ para ${nombreA}${fechaA}";
+			returnVal=1
 		fi
 		#valida salida original iva
 		if [ ! -e "${pathECBSalida}${nombreA}${fechaA}ORIGINAL_IVA_${timeStampA}.TXT" ] ; then
-			echo "Error:falta archivo de salida ORIGINAL_IVA_ para ${nombreA}${fechaA}, se detiene el proceso";
-			exit -1
+			echo "Error:falta archivo de salida ORIGINAL_IVA_ para ${nombreA}${fechaA}";
+			returnVal=1
 		fi
 	fi
+	return $returnVal
 }
 
 #valida catalogos
 if [ ! -e "${pathECBCatalogos}pampaConceptos.TXT" ] || [ ! -e "${pathECBCatalogos}carterConceptos.TXT" ] ;
 then
-	echo "ERROR: Falta alguno de los archivos de catalogos"
-	exit -1
+	echo "ERROR: Falta alguno de los archivos de catalogos, se detiene el proceso"
+	exit 1
 fi
 
 echo "Iniciando tareas iva process para ecb $nombre" >> massive.log 
-java -classpath /home/linuxlite/shell_scripts/ECBIVA/javaBin/ main.FormateaECB $nombres $fecha $timestamp >> massive.log 2> massive.err 
+#java -classpath /home/linuxlite/shell_scripts/ECBIVA/javaBin/ main.FormateaECB $nombres $fecha $timestamp >> massive.log 2> massive.err 
 
 #valida salidas de formatea
 interfaces=(`echo $nombres | tr ',' ' '`)
 for ARG in ${interfaces[@]}
 do
 	validaSalidas ${ARG} $fecha $timestamp
+	valNumResult=$?
+	if [[ $valNumResult -eq 1 ]] ; then
+		echo "Error:error en validacion de salidas, se detiene el proceso"
+		exit 1
+	fi
 done
 
 
